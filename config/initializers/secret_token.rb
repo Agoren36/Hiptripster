389e3a3a300026d1e24f2ab434dbc50c88e3db68
Avi Goren <gorenavi1@gmail.com>
2013-12-08 16:34:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiptripster::Application.config.secret_key_base = '89025ac69478474a76f65b282e5dca08b49a982f6d941215f64ed0aac4016949f3f9b1a0d386491692db02cb3bca1fd9a17fec3afc876e33b8a1516ded921510'
